76ed516bf0eba45cc0d2cac376e15e104521a205
chandresh sankhala <chandresh@auza.in>
2026-06-29 19:00:42 +0530

    fix(branding): skip enrichment for disposable and free email domains

--- app/jobs/account/branding_enrichment_job.rb
+++ app/jobs/account/branding_enrichment_job.rb
@@ -2,6 +2,11 @@ class Account::BrandingEnrichmentJob < ApplicationJob
   queue_as :low
 
   def perform(account_id, email)
+    if disposable_or_free_email?(email)
+      Rails.logger.info "[BrandingEnrichment] Skipping enrichment for generic/disposable email: #{email}"
+      return
+    end
+
     result = WebsiteBrandingService.new(email).perform
     if result.blank?
       Rails.logger.info "[BrandingEnrichment] Enrichment failed for account=#{account_id} email=#{email}"
@@ -17,6 +22,21 @@ class Account::BrandingEnrichmentJob < ApplicationJob
   end
 
   private
+
+  def disposable_or_free_email?(email)
+    return true if email.blank?
+
+    address = ValidEmail2::Address.new(email)
+    return true if address.disposable?
+
+    domain = email.split('@').last&.downcase&.strip
+    free_domains = %w[
+      gmail.com yahoo.com hotmail.com outlook.com aol.com live.com icloud.com
+      mail.com protonmail.com proton.me yandex.com gmx.com zohomail.com zoho.com
+      yopmail.com mailinator.com tempmail.com
+    ]
+    free_domains.include?(domain)
+  end
 
   def finish_enrichment(account_id)
     Redis::Alfred.delete(format(Redis::Alfred::ACCOUNT_ONBOARDING_ENRICHMENT, account_id: account_id))
